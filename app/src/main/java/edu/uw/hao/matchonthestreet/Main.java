85df30238ed05800a9233754852e407b58a19fab
Hao Liu <chakalakafrank@gmail.com>
2016-01-26 00:35:08 -0800

    * init camera zoomed into UW

--- app/src/main/java/edu/uw/hao/matchonthestreet/Main.java
+++ app/src/main/java/edu/uw/hao/matchonthestreet/Main.java
@@ -56,8 +56,8 @@ public class Main extends AppCompatActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng uw = new LatLng(47.655292, -122.303144);
+        mMap.addMarker(new MarkerOptions().position(uw).title("Marker in UW"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(uw, 18));
     }
 }
